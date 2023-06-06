51ff4195a1fc152725a1c61a100633244b13e77d
Adrian Muhammad Kuncoro <57836873+dakochan666@users.noreply.github.com>
2023-06-06 19:19:10 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+function toRoman(num) {
+ // Tuliskan kode disini
+}
+
+// Drive code
+console.log("My totally sweet testing script for new roman\n");
+console.log("input | expected | actual");
+console.log("——————|——————————|———————");
+console.log("4     | IV       | ", toRoman(4));
+console.log("9     | IX       | ", toRoman(9));
+console.log("13    | XIII     | ", toRoman(13));
+console.log("1453  | MCDLIII  | ", toRoman(1453));
+console.log("1646  | MDCXLVI  | ", toRoman(1646));
