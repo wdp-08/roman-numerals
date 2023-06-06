b6d4533a775db12f5ebcb61ef1e9cba564b96f5f
Delin Herliana Pashs <delinherlianapasha@gmail.com>
2023-06-06 21:21:57 +0700

    add roman num

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 function toRoman(num) {
- // Tuliskan kode disini
+  // Tuliskan kode disini
+  const romanNumerals = [
+    { desimal: 1000, romawi: "M" },
+    { desimal: 900, romawi: "CM" },
+    { desimal: 500, romawi: "D" },
+    { desimal: 400, romawi: "CD" },
+    { desimal: 100, romawi: "C" },
+    { desimal: 90, romawi: "XC" },
+    { desimal: 50, romawi: "L" },
+    { desimal: 40, romawi: "XL" },
+    { desimal: 10, romawi: "X" },
+    { desimal: 9, romawi: "IX" },
+    { desimal: 5, romawi: "V" },
+    { desimal: 4, romawi: "IV" },
+    { desimal: 1, romawi: "I" },
+  ];
+
+  let hasil = "";
+
+  for (let i = 0; i < romanNumerals.length; i++) {
+    while (num >= romanNumerals[i].desimal) {
+      hasil += romanNumerals[i].romawi;
+      num -= romanNumerals[i].desimal;
+    }
+  }
+
+  return hasil;
 }
 
 // Drive code
