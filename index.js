b7be2b78947b65ad265f7652292105344e5a9cd4
Dodistywn <dodisetya000@gmail.com>
2023-06-06 20:46:07 +0700

    add index.js

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 function toRoman(num) {
- // Tuliskan kode disini
+    let roman = {
+        I : 1,
+        IV : 4,
+        V : 5,
+        IX : 9,
+        X : 10,
+        XL : 40,
+        L : 50,
+        XC : 90,
+        C : 100,
+        CD : 400,
+        D : 500,
+        CM : 900,
+        M : 1000
+    }
+    let str = '';
+
+    for (let i of Object.keys(roman)) {
+        var q = Math.floor(num / roman[i]);
+        num -= q * roman[i];
+        str += i.repeat(q);
+    }
+    return str;
 }
 
 // Drive code
